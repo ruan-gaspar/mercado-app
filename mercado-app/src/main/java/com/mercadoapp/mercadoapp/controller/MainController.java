f08284b260d858308da22550add64406aa4bb81d
Ruan Gaspar <179716912+RuanGaspar-TDSPA@users.noreply.github.com>
2026-04-04 02:39:51 -0300

    Primeiro protótipo das telas criado

--- mercado-app/src/main/java/com/mercadoapp/mercadoapp/controller/MainController.java
+++ mercado-app/src/main/java/com/mercadoapp/mercadoapp/controller/MainController.java
@@ -6,37 +6,68 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainController {
+
     @FXML
-    private Button categoryButton;
+    private Button plannedOrderButton;
+
     @FXML
-    private Button sectorButton;
+    private Button plannedOrderItemButton;
+
+    @FXML
+    private Button orderButton;
+
+    @FXML
+    private Button orderItemButton;
+
     @FXML
     private Button productButton;
+
+    @FXML
+    private Button categoryButton;
+
+    @FXML
+    private Button sectorButton;
+
+    @FXML
+    protected void openPlannedOrderScreen() {
+        changeScene("/com/mercadoapp/mercadoapp/fxml/planned-order-view.fxml", plannedOrderButton);
+    }
+
+    @FXML
+    protected void openPlannedOrderItemScreen() {
+        changeScene("/com/mercadoapp/mercadoapp/fxml/planned-order-item-view.fxml", plannedOrderItemButton);
+    }
+
     @FXML
     protected void openOrderScreen() {
-        System.out.println("Compras");
+        changeScene("/com/mercadoapp/mercadoapp/fxml/order-view.fxml", orderButton);
     }
+
     @FXML
-    protected void openInventoryScreen() {
-        System.out.println("Inventário");
+    protected void openOrderItemScreen() {
+        changeScene("/com/mercadoapp/mercadoapp/fxml/order-item-view.fxml", orderItemButton);
     }
+
     @FXML
-    protected void openProductDetailScreen() {
+    protected void openProductScreen() {
         changeScene("/com/mercadoapp/mercadoapp/fxml/product-view.fxml", productButton);
     }
+
     @FXML
     protected void openCategoryScreen() {
         changeScene("/com/mercadoapp/mercadoapp/fxml/category-view.fxml", categoryButton);
     }
+
     @FXML
     protected void openSectorScreen() {
         changeScene("/com/mercadoapp/mercadoapp/fxml/sector-view.fxml", sectorButton);
     }
 
-
     private void changeScene(String fxmlPath, Button button) {
-        try{
+        try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Scene scene = new Scene(loader.load());
 
@@ -44,9 +75,8 @@ public class MainController {
             stage.setScene(scene);
             stage.show();
 
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao abrir a tela: " + fxmlPath, e);
         }
     }
-
 }
